fae9ad7aafbd9dabca7a0f75ec469d11ce6da324
sudiptabiswas <asksudipta2012@gmail.com>
2026-02-07 22:56:35 +0530

    more algos

--- src/dsa/ListNode.java
+++ src/dsa/ListNode.java
@@ -3,9 +3,7 @@ package dsa;
 public class ListNode {
     int val;
     public ListNode next;
-    ListNode() {}
     public ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
     public static void printList(ListNode node) {
         while (node != null) {
